8a9415b64f024bd2af1595a10bb4f98de2c52458
rodri-dot-git <rodri230998@gmail.com>
2020-06-04 13:12:38 -0500

    clientcredentialsgrant

--- spotify/api.js
+++ spotify/api.js
@@ -6,8 +6,15 @@ const spotifyApi = new SpotifyWebApi({
     clientSecret: process.env.SPOTIFY_SECRET,
 });
 
-module.exports.callback = function callback(code) {
-    spotifyApi.authorizationCodeGrant(code)
+module.exports.callback = async function callback(code) {
+    await spotifyApi.clientCredentialsGrant()
+    .then((data) => {
+        console.log('The access token is ' + data.body['access_token']);
+        spotifyApi.setAccessToken(data.body['access_token']);
+        spotifyApi.setRefreshToken(data.body['refresh_token']);
+    })
+    .catch((error) => console.log(error))
+    await spotifyApi.authorizationCodeGrant(code)
     .then((data) => {
         console.log(data)
         spotifyApi.setAccessToken(data.body['access_token']);
