3ccd9749d1f220a9c2e48918d4dd70fb745b4047
rodri-dot-git <rodri230998@gmail.com>
2020-06-04 13:51:18 -0500

    ya funciona

--- spotify/api.js
+++ spotify/api.js
@@ -8,28 +8,28 @@ const spotifyApi = new SpotifyWebApi({
 
 module.exports.callback = async function callback(code) {
     await spotifyApi.clientCredentialsGrant(code)
-    .then((data) => {
+    .then(async (data) => {
         console.log('The access token is ' + data.body['access_token']);
-        spotifyApi.setAccessToken(data.body['access_token']);
-        spotifyApi.setRefreshToken(data.body['refresh_token']);
+        await spotifyApi.setAccessToken(data.body['access_token']);
+        await spotifyApi.setRefreshToken(data.body['refresh_token']);
     })
     .catch((error) => console.log(error))
 
-    spotifyApi.getMe()
+    await spotifyApi.getMe()
     .then((data) => console.log(data.body))
     .catch((error) => console.log(error))
 
     let date = new Date();
 	date.setHours(date.getHours() - 7)
 
-    spotifyApi.getFeaturedPlaylists({
+    await spotifyApi.getFeaturedPlaylists({
         limit: 15,
         offset: 0,
         country: 'US',
         timestamp: date
     })
     .then(function (data) {
-        console.log('popular', data.body.playlists.items)
+        //console.log('popular', data.body.playlists.items)
     }, function (err) {
         console.log("Something went wrong!", err);
     })
