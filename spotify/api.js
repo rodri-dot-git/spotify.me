cc10bf1d711b7dafc8453df9711a11d1300948ce
rodri-dot-git <rodri230998@gmail.com>
2020-06-04 13:53:54 -0500

    wooork

--- spotify/api.js
+++ spotify/api.js
@@ -9,12 +9,13 @@ const spotifyApi = new SpotifyWebApi({
 module.exports.callback = async function callback(code) {
     await spotifyApi.clientCredentialsGrant(code)
     .then(async (data) => {
-        console.log('The access token is ' + data.body['access_token']);
-        await spotifyApi.setAccessToken(data.body['access_token']);
+        await spotifyApi.setAccessToken(data.body['access_token'])
+        .then(() => {
+            spotifyApi.getMe()
+            .then((data) => console.log(data.body))
+            .catch((error) => console.log(error))
+        })
         await spotifyApi.setRefreshToken(data.body['refresh_token']);
-        await spotifyApi.getMe()
-        .then((data) => console.log(data.body))
-        .catch((error) => console.log(error))
     })
     .catch((error) => console.log(error))
 
