cfa87678532645513b7866f762028844452f50b7
rodri-dot-git <rodri230998@gmail.com>
2020-06-04 13:31:33 -0500

    fix

--- spotify/api.js
+++ spotify/api.js
@@ -7,16 +7,18 @@ const spotifyApi = new SpotifyWebApi({
 });
 
 module.exports.callback = async function callback(code) {
-    await spotifyApi.clientCredentialsGrant()
+    await spotifyApi.authorizationCodeGrant(code)
     .then((data) => {
-        console.log('The access token is ' + data.body['access_token']);
+        console.log(data.body)
         spotifyApi.setAccessToken(data.body['access_token']);
         spotifyApi.setRefreshToken(data.body['refresh_token']);
     })
     .catch((error) => console.log(error))
+
     spotifyApi.getMe()
     .then((data) => console.log(data.body))
     .catch((error) => console.log(error))
+    
     spotifyApi.getFeaturedPlaylists({
         limit: 15,
         offset: 0,
