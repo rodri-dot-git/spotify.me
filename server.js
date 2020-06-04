ee8aa3ec54baccffca85993c635e8d6ea57bbaa2
rodri-dot-git <rodri230998@gmail.com>
2020-06-04 12:01:57 -0500

    basic structure

---
+++ server.js
@@ -0,0 +1,26 @@
+//#region imports
+const bodyParser = require("body-parser");
+const path = require("path");
+const dotenv = require("dotenv");
+const express = require("express");
+dotenv.config();
+const app = express();
+const http = require("http").createServer(app);
+
+//#endregion
+
+//#region app use
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/public", express.static(path.join(__dirname, "/public")));
+//#endregion
+
+//#region rutas get
+app.get("/index", (req, res) => {
+	res.sendFile(path.join(__dirname, "public/views/index.html"));
+});
+//#endregion
+
+http.listen(process.env.PORT || 4000, () => {
+	console.log("🚀🚀 Server ready");
+});
